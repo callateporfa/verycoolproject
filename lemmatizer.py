d5fa008dc28515d1123d0cb5d376dd08f28dd404
4erepash <71963013+4erepash@users.noreply.github.com>
2021-05-13 12:10:10 +0300

    Update and rename normalizer.py to lemmatizer.py

--- lemmatizer.py
+++ lemmatizer.py
@@ -1,14 +1,16 @@
 import json
 import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
+
+#пробная программа которая создает словарь вида {'паблик':[список всех лемм (не сет)]}
 with open('results.json', encoding='utf-8') as f:
     json_dict = json.load(f)
 for key in json_dict:
-    blin = []
+    list_of_lemmas = []
     for value in json_dict[key]:
         value = value.split(' ')
         for word in value:
             word = morph.parse(word)[0].normal_form
-            blin.append(word)
+            list_of_lemmas.append(word)
     json_dict[key] = blin
 print(json_dict)
