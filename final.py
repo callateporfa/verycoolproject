083dfc77a7009f7c3a876cf0100a2f928ea8a715
arinazv <71773293+arinazv@users.noreply.github.com>
2021-05-13 12:43:37 +0300

    Update final.py

--- final.py
+++ final.py
@@ -12,7 +12,7 @@ os.chdir(sys.path[0])
 
 morph = pymorphy2.MorphAnalyzer()
 
-#делаем удобный для работы файл
+# делаем удобный для работы файл
 with open('results.json', encoding='utf-8') as f:
     json_dict = json.load(f)
 for key in json_dict:
@@ -20,13 +20,13 @@ for key in json_dict:
     for value in json_dict[key]:
         value = value.split(' ')
         for word in value:
-            word = morph.parse(word)[0].normal_form
-            word = word.strip(string.punctuation).lower()
+            word = morph.parse(word)[0].normal_form  # приводим слова к начальной форме
+            word = word.strip(string.punctuation).lower()  # убираем пунктуацию и заглавные буквы
             blin.append(word)
     json_dict[key] = blin
 
-# сделали
-with open('new_res.json', 'w', encoding = 'utf-8') as new:
+# сделали и сохраняем в формате json
+with open('new_res.json', 'w', encoding='utf-8') as new:
     json.dump(json_dict, new, ensure_ascii=False, indent=2)
 
 
@@ -41,10 +41,10 @@ for key in json_dict:
         for word in value:
             c[word] += 1
     json_dict[key] = c
-    for i in json_dict[key].most_common(100):
+    for i in json_dict[key].most_common(10000):  # собираем список самых частотных слов в каждом паблике
         for word in i:
             if isinstance(word, str) is True:
-                most_common_total.append(word)
+                most_common_total.append(word)  # готовый лист стоп-слов
 
 # нашли
 res = []
@@ -55,17 +55,17 @@ res = list(set(res))
 
 new = open('new_res.json', 'r', encoding='utf-8').read()
 for key in json_dict:
-    text = open(str(key) + '.json', 'w+', encoding= 'utf-8')
-    json.dump(json_dict[key], text, ensure_ascii=False, indent=2)
+    text = open(str(key) + '.json', 'w+', encoding='utf-8')
+    json.dump(json_dict[key], text, ensure_ascii=False, indent=2)  # json файл для каждого паблика
 
 for key in json_dict:
     text = open(str(key) + '.json', 'r', encoding='utf-8').read()
-    stoplist = list(STOPWORDS)+list(stopwords.words('russian'))+res
+    stoplist = list(STOPWORDS)+list(stopwords.words('russian'))+ res  # стоп-слова по умолчанию + русские стоп-слова + + стоп-слова наших данных
 
-    wc = WordCloud(
-        background_color = 'white',
-        stopwords = stoplist,
+    wc = WordCloud(                 # параметры облака слов
+        background_color='white',
+        stopwords=stoplist,
         height=1000,
-        width= 1500)
+        width=1500)
     wc.generate(text)
-    wc.to_file(str(key)+'.png')
+    wc.to_file(str(key) + '.png')  # сохраняем картику с названием паблика
