d35bedac8cd4915d7f12b54a220ef949f4db5648
4erepash <71963013+4erepash@users.noreply.github.com>
2021-05-14 16:20:52 +0300

    Update defining_public_probabilities_version.py

--- defining_public_probabilities_version.py
+++ defining_public_probabilities_version.py
@@ -28,62 +28,58 @@ for word in data:
             freq.append(0) 
     json_freq_dict[word] = freq
 
-    
 #основная часть
-names = []
-list_of_comments = list(input('Введите список комментариев: '))
-for data in list_of_comments:
-    data = data.split(' ')
-    for word in data:
-        word = word.strip(string.punctuation).lower()
-    #приводим слова в инпутном комментарии к лемме и если слово есть в нашем частотном словаре, то мы берем список частотностей в 12 пабликах и прибавляем его почленно к probabilities (похоже на сложение столбиком)
-    probabilities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for word in data:
-        word = morph.parse(word)[0].normal_form
-        if word in json_freq_dict: 
-            for i in range(12):
-                probabilities[i] += json_freq_dict[word][i] 
+data = input('Введите комментарий: ')
+data = data.split(' ')
+for word in data:
+    word = word.strip(string.punctuation).lower()
+#приводим слова в инпутном комментарии к лемме и если слово есть в нашем частотном словаре, то мы берем список частотностей в 12 пабликах и прибавляем его почленно к probabilities (похоже на сложение столбиком)
+probabilities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+for word in data:
+    word = morph.parse(word)[0].normal_form
+    if word in json_freq_dict: 
+        for i in range(12):
+            probabilities[i] += json_freq_dict[word][i] 
 
-    #каждую из полученных сумм вероятностей домножаем на коэффициент встречаемости паблика (т.е. количество слов из комментариев этого паблика в наших данных / сумма всех слов во всех комментариях)
-    probabilities[0] = probabilities[0]*3435/88033
-    probabilities[1] = probabilities[1]*11902/88033
-    probabilities[2] = probabilities[2]*11807/88033
-    probabilities[3] = probabilities[3]*7686/88033
-    probabilities[4] = probabilities[4]*6259/88033
-    probabilities[5] = probabilities[5]*3995/88033
-    probabilities[6] = probabilities[6]*7465/88033
-    probabilities[7] = probabilities[7]*18251/88033
-    probabilities[8] = probabilities[8]*5163/88033
-    probabilities[9] = probabilities[9]*3549/88033
-    probabilities[10] = probabilities[10]*5709/88033
-    probabilities[11] = probabilities[11]*2812/88033
+#каждую из полученных сумм вероятностей домножаем на коэффициент встречаемости паблика (т.е. количество слов из комментариев этого паблика в наших данных / сумма всех слов во всех комментариях)
+probabilities[0] = probabilities[0]*3435/88033
+probabilities[1] = probabilities[1]*11902/88033
+probabilities[2] = probabilities[2]*11807/88033
+probabilities[3] = probabilities[3]*7686/88033
+probabilities[4] = probabilities[4]*6259/88033
+probabilities[5] = probabilities[5]*3995/88033
+probabilities[6] = probabilities[6]*7465/88033
+probabilities[7] = probabilities[7]*18251/88033
+probabilities[8] = probabilities[8]*5163/88033
+probabilities[9] = probabilities[9]*3549/88033
+probabilities[10] = probabilities[10]*5709/88033
+probabilities[11] = probabilities[11]*2812/88033
 
-    #ищем номер паблика, у которого полученная вероятность получилась максимальной и присваиваем каждому номеру название
-    number = probabilities.index(max(probabilities)) + 1
-    if number == 1:
-        name = 'Типография НИУ ВШЭ'
-    if number == 2:
-        name = 'Медуза'
-    if number == 3:
-        name = 'Лентач'
-    if number == 4:  
-        name = 'Капризный ленгвист'
-    if number == 5: 
-        name = 'Постнаука'
-    if number == 6: 
-        name = 'CLIQUE'
-    if number == 7:
-        name = 'КБ '
-    if number ==  8:
-        name =  'калик)'
-    if number == 9:
-        name = 'абстрактные мемы для элиты всех сортов | АМДЭВС'
-    if number ==  10:
-        name = '4ch'
-    if number == 11:
-        name = 'реализм кухонной раковины'
-    if number ==  12:
-        name = 'астрофотография'
+#ищем номер паблика, у которого полученная вероятность получилась максимальной и присваиваем каждому номеру название
+number = probabilities.index(max(probabilities)) + 1
+if number == 1:
+    name = 'Типография НИУ ВШЭ'
+if number == 2:
+    name = 'Медуза'
+if number == 3:
+    name = 'Лентач'
+if number == 4:  
+    name = 'Капризный ленгвист'
+if number == 5: 
+    name = 'Постнаука'
+if number == 6: 
+    name = 'CLIQUE'
+if number == 7:
+    name = 'КБ '
+if number ==  8:
+    name =  'калик)'
+if number == 9:
+    name = 'абстрактные мемы для элиты всех сортов | АМДЭВС'
+if number ==  10:
+    name = '4ch'
+if number == 11:
+    name = 'реализм кухонной раковины'
+if number ==  12:
+    name = 'астрофотография'
 
-    names.append(name)
-print(names)
+print('Вероятно, это паблик номер ', number, ',', name)
